b57dc35dce555303eea4db6c94cc0d55c740fb89
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-08-28 22:51:41 +0530

    Array1st topic

---
+++ 01_basics/02_basics/Array01.js
@@ -0,0 +1,50 @@
+// const fruits = ["Apple", "Banana"];
+//    console.log(fruits.length);
+// //    console.log(fruits.push("Orange"));
+//    console.log(fruits);
+   
+//    const updatedFruits = fruits.reduce((arr, item) => {
+//   arr.push(item);
+//   return arr;
+// }, ["Orange"]);
+
+// console.log(updatedFruits); // 
+//  const arr = [1,2,3,6,9,10];
+//  console.log(arr.reverse());
+//  console.log(arr.shift());
+//  console.log(arr);
+
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 5));
+// console.log(arr.slice(-2));
+// const months = ["Jan", "March", "April", "June"];
+
+// console.log(months.splice(1,0,"Feb"));
+// console.log(months);
+// console.log(months.toString());
+
+// const arr = [1,2,3,6,9,10];
+// let index = 2 ;
+// console.log(`as index of ${index} returns ${arr.at(index)} `);
+
+// console.log(arr.concat(months.toString()));
+
+// console.log(months.includes("March"));
+const arr1 = ["1","3",["4","6"]];
+console.log(arr1.flat());
+
+const arr2 = [0, 1, [2, [3, [4, 5]]]];
+console.log(arr2.flat(2));
+
+
+
+
+
+
+
+ 
+ 
+ 
+
+
+    
