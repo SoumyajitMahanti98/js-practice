2eb2486b2ee1806422ad5ca73256ae1d31e114c5
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-09-02 23:02:11 +0530

    updated

--- 01_basics/02_basics/Array01.js
+++ 01_basics/02_basics/Array01.js
@@ -30,11 +30,29 @@
 // console.log(arr.concat(months.toString()));
 
 // console.log(months.includes("March"));
-const arr1 = ["1","3",["4","6"]];
-console.log(arr1.flat());
+// const arr1 = ["1","3",["4","6"]];
+// console.log(arr1.flat());
 
-const arr2 = [0, 1, [2, [3, [4, 5]]]];
-console.log(arr2.flat(2));
+// const arr2 = [0, 1, [2, [3, [4, 5]]]];
+// console.log(arr2.flat(2));
+
+// const myArray = [1,2,3,4,5];
+// const copyArray = myArray;
+// copyArray.push(6);
+// console.log(copyArray);
+// console.log(myArray);
+
+const myArray = [1,2,3,4,5];
+const copyArray = myArray.slice();
+copyArray.push(6);
+console.log(myArray);
+console.log(copyArray);
+
+
+
+
+
+
 
 
 
