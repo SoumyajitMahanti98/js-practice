185c1a91bd20ec47c706e550137c9e75a61f5f1a
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-09-05 20:22:23 +0530

    arrow fun

---
+++ 01_basics/02_basics/03_basics/03_arrow.js
@@ -0,0 +1,63 @@
+// const user = {
+//     username: "hitesh",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+
+// // user.welcomeMessage()
+// // user.username = "sam"
+// // user.welcomeMessage()
+
+// // console.log(this);
+
+// // function chai(){
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// // chai()
+
+// // const chai = function () {
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+
+// // chai()
+
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
+
+// // const addTwo = (num1, num2) =>  num1 + num2
+
+// // const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// // const myArray = [2, 5, 3, 7, 8]
+
+// // myArray.forEach()
+
+const addNumbers = (a,b) =>{      // explicit arrow function
+    return a+b;
+}
+console.log((addNumbers(5,3)));
+
+const add = (x,y) => (x+y);       // implicit arrow function 
+console.log((add(4,3)));
+
