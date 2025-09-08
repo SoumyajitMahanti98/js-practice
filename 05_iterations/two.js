8a038909581ccd15f44c194404ad07e11f14cb24
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-09-08 22:34:34 +0530

    while & do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+let result = "";
+let i = 0;
+do {
+  i += 1;
+  result += `${i} `;
+} while (i > 0 && i < 5);
+// Despite i === 0 this will still loop as it starts off without the test
+
+console.log(result);
