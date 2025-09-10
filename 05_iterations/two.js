a1039f25dfebaf3e334d243595beaef69104fb4d
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-09-10 23:04:49 +0530

    update

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -1,24 +1,24 @@
 
-// let index = 0
-// while (index <= 10) {
-//     console.log(`Value of index is ${index}`);
-//     index = index + 2
-// }
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
 
-// let myArray = ['flash', "batman", "superman"]
+let myArray = ['flash', "batman", "superman"]
 
-// let arr = 0
-// while (arr < myArray.length) {
-//     //console.log(`Value is ${myArray[arr]}`);
-//     arr = arr + 1
-// }
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
 
-// let score = 11
+let score = 11
 
-// do {
-//     console.log(`Score is ${score}`);
-//     score++
-// } while (score <= 10);
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
 
 let result = "";
 let i = 0;
@@ -29,3 +29,4 @@ do {
 // Despite i === 0 this will still loop as it starts off without the test
 
 console.log(result);
+
