764fe6567fd163d6bfe8fe8cee1e9f3ae5d73e3c
Soumyajit Mahanti <soumyajitmahanti98@gmail.com>
2025-10-28 00:48:40 +0530

    updated

---
+++ async&await/script.js
@@ -0,0 +1,23 @@
+// function resolveAfter2seconds(){
+//     return new Promise(resolve=>{
+//         setTimeout(()=>{
+//  console.log("resolved");
+//         },2000)
+//     })
+// }
+// async function asyncCall(){
+//     console.log("calling..");
+//     const result = await resolveAfter2seconds();
+//     console.log(result);
+    
+// }
+// asyncCall();
+
+/// another example...
+async function makeasyncRequest(){
+    const url = 'https://jsonplaceholder.typicode.com/albums/1/photos';
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+}
+makeasyncRequest();
